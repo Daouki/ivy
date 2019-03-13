34bc05a9582544b5285bd261d1d2564d7d6e601d
Daouki <daouki97@gmail.com>
2019-03-13 19:57:53 +0100

    Setup project files.

---
+++ Ivy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ivy
+{
+    internal static class IvyInterpreter
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
